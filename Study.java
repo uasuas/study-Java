cba4417b3afdeb341cbfe23f3661d4e6abc94a95
usausa <143808163+uasuas@users.noreply.github.com>
2024-01-27 19:08:35 +0900

    [update] ページ数記入

--- Study.java
+++ Study.java
@@ -2,6 +2,7 @@ package VSC.Java.Java_study;
 
 public class Study {
   public static void main(String[] args) {
+    // 改行。68p
     // ¥nで改行できずoption+¥の\とnで改行になった。
     String name = "名前：testname\n   (テスト)";
     String name2 = """
@@ -11,13 +12,13 @@ public class Study {
     System.out.println(name);
     System.out.println(name2);
 
-    // ++aとa++の微妙な違い。
+    // ++aとa++の微妙な違い。78p
     int a1 = 10;
     int a2 = 10;
     System.out.println(a1++ + 50); //出力60
     System.out.println(++a2 + 50); //出力61
 
-    // 文字列を整数に変換。
+    // 文字列を整数に変換。93p
     String age = "30";
     int age2 = Integer.parseInt(age);
     System.out.println(age2 + 1);
