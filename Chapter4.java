e302823781d4b02eb852bd1b43789616f2f1d0a4
usausa <143808163+uasuas@users.noreply.github.com>
2024-01-31 19:01:13 +0900

    [update] 配列

---
+++ Chapter4.java
@@ -0,0 +1,15 @@
+package VSC.Java.Java_study;
+
+public class Chapter4 {
+  public static void main(String[] args) {
+    // 配列の作成手順。144p
+    // 短縮前
+    // int[] scores;
+    // scores = new int[5];
+    // 短縮型
+    int[] scores = new int[5];
+    // 配列の要素数を取得
+    int num = scores.length;
+    System.out.println(num);
+  }
+}
