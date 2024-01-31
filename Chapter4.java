bf20cfd842e0c57320e1230a56a8825d4156ba2e
usausa <143808163+uasuas@users.noreply.github.com>
2024-01-31 19:17:40 +0900

    [update] 配列の平均

--- Chapter4.java
+++ Chapter4.java
@@ -17,5 +17,11 @@ public class Chapter4 {
     // 配列の作成と初期値。147p
     int[] scores1 = {10, 20, 30, 40, 50};
     System.out.println(scores1[2]);
+
+    // 平均の算出。148p
+    int[] scores2 = {20, 30, 50, 100, 100};
+    int sum = scores2[0] + scores2[1] + scores2[2] + scores2[3] + scores2[4];
+    int avg = sum / scores2.length;
+    System.out.println("平均:" + avg);
   }
 }
