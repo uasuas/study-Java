1e13629332f6556ef1dbb8f232d551846604c320
usausa <143808163+uasuas@users.noreply.github.com>
2024-02-02 18:30:07 +0900

    [add] Chapter5作成

---
+++ Chapter5.java
@@ -0,0 +1,10 @@
+package VSC.Java.Java_study;
+
+public class Chapter5 {
+  public static void main(String[] args) {
+    hello();
+  }
+  public static void hello() {
+    System.out.println("こんにちは");
+  }
+}
