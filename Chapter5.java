3b71f8367ede9c49c87e1935a6743c16a10c984f
usausa <143808163+uasuas@users.noreply.github.com>
2024-02-03 22:13:38 +0900

    [update] 引数について

--- Chapter5.java
+++ Chapter5.java
@@ -1,10 +1,21 @@
 package VSC.Java.Java_study;
 
 public class Chapter5 {
+  // メソットの呼び出し。178p
   public static void main(String[] args) {
     hello();
+    A();
   }
   public static void hello() {
     System.out.println("こんにちは");
+  }
+  // mein外からのメソッドの呼び出し。179p
+  public static void A() {
+    System.out.println("A");
+    B("メソッドB");
+  }
+  // 引数にて内容を変更する。184p
+  public static void B(String name) {
+    System.out.println(name + "というメソッドです");
   }
 }
